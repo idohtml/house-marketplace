6dc181066c146ab62ee21df58da7e137555a0afa
Jacob <jacob@archlinux.localdomain>
2023-10-18 01:21:48 +0200

    Google auth done

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import googleIcon from "../assets/svg/googleIcon.svg";
@@ -16,17 +16,22 @@ function OAuth() {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
 
+      // Check for user
       const docRef = doc(db, "users", user.uid);
       const docSnap = await getDoc(docRef);
 
+      // if user doesnt exist, create user
       if (!docSnap.exists()) {
         await setDoc(doc(db, "users", user.uid), {
           name: user.displayName,
           email: user.email,
-          timestamp: new Date(),
+          timestamp: serverTimestamp(),
         });
       }
-    } catch (error) {}
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
   };
 
   return (
